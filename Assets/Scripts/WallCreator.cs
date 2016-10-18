59ca613f7571599bd95eee45c7ebcf19f0c41f7e
Taimoor Rana <taimoorrana1@hotmail.com>
2016-10-17 20:38:59 -0400

    Added Folder Structure and Basic gun functionality

---
+++ Assets/Scripts/WallCreator.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WallCreator : MonoBehaviour {
+
+	[SerializeField] GameObject m_Block;
+	[SerializeField] float m_BlockSpeed;
+	// Use this for initialization
+	void Start () {
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+
+	}
+
+	void OnCollisionEnter(Collision other) {
+		print("Points colliding: " + other.contacts.Length);
+		print("First point that collided: " + other.contacts[0].point);
+
+		GameObject blockCopy = Instantiate(m_Block);
+		blockCopy.transform.position = other.contacts[0].point;
+
+		blockCopy.GetComponent<Rigidbody>().AddForce(transform.up * m_BlockSpeed);
+		// destroying the bullet
+		Destroy(other.gameObject);
+		Destroy (blockCopy, 5f);
+	}
+}
