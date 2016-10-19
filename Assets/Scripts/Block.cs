338bdb5a99a4f2a184239395f7cea54b841b2674
Taimoor Rana <taimoorrana1@hotmail.com>
2016-10-18 23:46:39 -0400

    transfrom.forward was not match the rotation of the wall. transform.localRotation assigns the correct rottation

--- Assets/Scripts/Block.cs
+++ Assets/Scripts/Block.cs
@@ -4,7 +4,9 @@ using System.Collections;
 public class Block : MonoBehaviour {
 	[SerializeField] private float m_ScaleSpeed;
 
+
 	void OnMouseDrag(){
-		transform.localScale += transform.forward * m_ScaleSpeed;
+		Vector3 stretchY = new Vector3(0,1,0) * m_ScaleSpeed;
+		transform.localScale += stretchY;
 	}
 }
