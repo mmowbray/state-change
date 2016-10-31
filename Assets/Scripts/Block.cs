0b09da59c65e703584377975c76809b60ada6373
Taimoor Rana <taimoorrana1@hotmail.com>
2016-10-30 22:33:29 -0400

    reverting before jarret stuff

---
+++ Assets/Scripts/Block.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Block : MonoBehaviour {
+	[SerializeField] private float m_ScaleSpeed;
+
+	public Vector3 wallPoint;
+
+	void OnMouseDrag(){
+		Vector3 stretchY = new Vector3(0,1,0) * m_ScaleSpeed;
+		transform.localScale += stretchY;
+	}
+}
---
+++ Assets/Scripts/PlayerExtension.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerExtension : MonoBehaviour {
+	[SerializeField] private GameObject m_BulletPrefab;
+	[SerializeField] private Transform m_BulletSpawnLocation;
+	[SerializeField] private float m_BulletSpeed;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if (Input.GetKeyDown (KeyCode.Mouse0)) {
+			GameObject bulletCopy = Instantiate (m_BulletPrefab) as GameObject;
+			bulletCopy.transform.position = m_BulletSpawnLocation.position;
+			bulletCopy.GetComponent<Rigidbody> ().velocity = m_BulletSpawnLocation.forward * m_BulletSpeed;
+		}
+	}
+}
