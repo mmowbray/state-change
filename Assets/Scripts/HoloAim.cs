4209fc4c4a2362f0d4e18cd5591cbead96eccaa2
Taimoor Rana <taimoorrana1@hotmail.com>
2016-11-07 22:16:01 -0500

    minor refactoring

--- Assets/Scripts/HoloAim.cs
+++ Assets/Scripts/HoloAim.cs
@@ -3,19 +3,21 @@ using System.Collections;
 
 public class HoloAim : MonoBehaviour
 {
+	[SerializeField] private float m_ScaleSpeed;
+	[SerializeField] float m_FixedLength;
+
     public GameObject holoBlock;
 	public GameObject realBlock;
     public GameObject aimer;
 
-    [SerializeField] private float m_ScaleSpeed;
-	[SerializeField] float m_fixedLength;
     private GameObject highlightedBlock;
 	private bool targetting;
 	private ParticleSystem chargeEffects;
 	private float charge;
 	private bool ignoreMouse0KeyUp;
 	private bool isPuzzleMode = false;
-    
+	private RaycastHit hit; //where the intersection is in world coords
+
     void Start ()
     {
 		holoBlock.SetActive (false);
@@ -26,25 +28,15 @@ public class HoloAim : MonoBehaviour
 
 	void Update()
 	{
-		GunMode ();
+		GunMode (); 
+
 		if (Input.GetButtonDown ("ActivateAiming"))
 			targetting = !targetting;
 
 		holoBlock.SetActive (targetting);
 
-		if (Input.GetKey (KeyCode.Mouse0)) {
-			charge += Time.fixedDeltaTime;
+		ChargeGun ();
 
-			if (Input.GetKey (KeyCode.Mouse1)) {
-				charge = 0f;
-			}
-
-			if (!chargeEffects.isPlaying) {
-				chargeEffects.Play ();
-			}
-		}
-
-		RaycastHit hit; //where the intersection is in world coords
 
 		if (Physics.Raycast (transform.position, aimer.transform.forward, out hit)) { //there was a collision with something in the scene
 
@@ -53,7 +45,7 @@ public class HoloAim : MonoBehaviour
 				if (isPuzzleMode == false) {
 					holoBlock.transform.localScale = charge > 0 ? new Vector3 (1.0f, 1.0f + charge * m_ScaleSpeed, 1.0f) : Vector3.one;
 				} else {
-					holoBlock.transform.localScale = new Vector3 (1.0f,m_fixedLength , 1.0f);
+					holoBlock.transform.localScale = new Vector3 (1.0f,m_FixedLength , 1.0f);
 				}
 				holoBlock.transform.position = hit.point;
 				holoBlock.transform.rotation = hit.transform.gameObject.transform.rotation;
@@ -75,19 +67,39 @@ public class HoloAim : MonoBehaviour
 					
 			}
 
-			if (Input.GetKeyUp (KeyCode.Mouse0)) {
-				if (ignoreMouse0KeyUp == true) { // Mouse0 KeyUp needs to be ignored if Mouse0 was pressed to delete the block
-					ignoreMouse0KeyUp = false; 
-				} else {
-					GameObject newBlock = Instantiate (realBlock, holoBlock.transform.position, holoBlock.transform.rotation) as GameObject;
-					newBlock.transform.localScale = holoBlock.transform.localScale;
+			createBlock ();
 
-					charge = 0; //reset the charge
-					chargeEffects.Stop ();
-				}
-			}
 		}
     }
+
+	void createBlock(){
+		
+		if (Input.GetKeyUp (KeyCode.Mouse0)) {
+			if (ignoreMouse0KeyUp == true) { // Mouse0 KeyUp needs to be ignored if Mouse0 was pressed to delete the block
+				ignoreMouse0KeyUp = false; 
+			} else {
+				GameObject newBlock = Instantiate (realBlock, holoBlock.transform.position, holoBlock.transform.rotation) as GameObject;
+				newBlock.transform.localScale = holoBlock.transform.localScale;
+
+				charge = 0; //reset the charge
+				chargeEffects.Stop ();
+			}
+		}
+	}
+
+	void ChargeGun(){
+		if (Input.GetKey (KeyCode.Mouse0)) {
+			charge += Time.fixedDeltaTime;
+
+			if (Input.GetKey (KeyCode.Mouse1)) {
+				charge = 0f;
+			}
+
+			if (!chargeEffects.isPlaying) {
+				chargeEffects.Play ();
+			}
+		}
+	}
 
 	void GunMode (){
 		if (Input.GetKeyDown (KeyCode.Q)) {
