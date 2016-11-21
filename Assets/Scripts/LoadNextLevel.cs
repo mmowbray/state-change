51c1bc1c42ef46e171cc4efa59ccd280e7f877b3
Jumper706 <jarwen706@hotmail.com>
2016-11-20 23:24:11 -0500

    Started the second level and started the code to transition between levels

---
+++ Assets/Scripts/LoadNextLevel.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+
+public class LoadNextLevel : MonoBehaviour
+{
+    private GameObject player;
+    public float openDist;
+    public float passDist;
+    public string level;
+
+    // Use this for initialization
+    void Start ()
+    {
+        player = GameObject.FindGameObjectWithTag("Player");
+    }
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        if (Vector3.Distance(transform.position, player.transform.position) <= openDist && player.transform.position.z >= transform.position.z + passDist)
+        {
+            Debug.Log("Loading");
+            SceneManager.LoadScene(level, LoadSceneMode.Single);
+        }
+
+    }
+}
