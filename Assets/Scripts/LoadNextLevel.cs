a2806c66d5e3031f85af3eac39935500b60f1fe7
Jumper706 <jarwen706@hotmail.com>
2016-11-28 13:26:07 -0500

    Fixed up the tutorial messages and level transition

--- Assets/Scripts/LoadNextLevel.cs
+++ Assets/Scripts/LoadNextLevel.cs
@@ -6,7 +6,6 @@ public class LoadNextLevel : MonoBehaviour
 {
     private GameObject player;
     public float openDist;
-    public float passDist;
     public string level;
 
     // Use this for initialization
@@ -18,7 +17,7 @@ public class LoadNextLevel : MonoBehaviour
 	// Update is called once per frame
 	void Update ()
     {
-        if (Vector3.Distance(transform.position, player.transform.position) <= openDist/* && player.transform.position.z >= transform.position.z + passDist*/)
+        if (Vector3.Distance(transform.position, player.transform.position) <= openDist)
         {
             Debug.Log("Loading");
             SceneManager.LoadScene(level, LoadSceneMode.Single);
